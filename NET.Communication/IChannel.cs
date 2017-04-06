0844e72d8ea6af176153d219884370d21d7f2a28
chao <suchao@jingge.com>
2017-04-06 17:11:46 +0800

    first_commit

---
+++ NET.Communication/IChannel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.Communication
+{
+    public interface IChannel
+    {
+        /// <summary>
+        /// 优先级
+        /// </summary>
+        int Priority { get;}
+        /// <summary>
+        /// 通信类型名
+        /// </summary>
+        string Name { get; }
+        /// <summary>
+        /// 读取
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        T Read<T>();
+        /// <summary>
+        /// 写入
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        T Write<T>();
+    }
+}
---
+++ NET.Communication/WebChannel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.Communication
+{
+    public class WebChannel : IChannel
+    {
+        public WebChannel()
+        {
+            this.Name = "Web";
+            this.Priority = 0;
+        }
+        public string Name { get; private set; }
+        public int Priority { get; private set; }
+
+        public T Read<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Write<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
