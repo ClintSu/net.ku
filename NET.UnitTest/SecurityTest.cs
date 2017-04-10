5d4a6f05242ec37f0a75462fba0acc599b842795
chao <suchao@jingge.com>
2017-04-10 14:23:12 +0800

    remark

--- NET.UnitTest/SecurityTest.cs
+++ NET.UnitTest/SecurityTest.cs
@@ -13,19 +13,19 @@ namespace NET.UnitTest
             string publicKey = string.Empty;
             string content = @"123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567";
             NET.Security.Rsa rsa= new NET.Security.Rsa();
-            rsa.RsaKey(out privateKey,out publicKey);
-            var encrypted = rsa.Encrypt(publicKey, content);
-            var decrypted = rsa.Decrypt(privateKey, encrypted);
+            rsa.RsaKey(out privateKey,out publicKey); //生成密钥
+            var encrypted = rsa.Encrypt(publicKey, content);  //值加密
+            var decrypted = rsa.Decrypt(privateKey, encrypted); //值解密
             Assert.AreEqual(content, decrypted);
 
-            string sprivateKey = string.Empty;
-            string spublicKey = string.Empty;
+            string sPrivateKey = string.Empty;
+            string sPublicKey = string.Empty;
             string hashContent = string.Empty;
             string hashContentSignature = string.Empty;
-            rsa.RsaKey(out sprivateKey,out spublicKey);
-            rsa.GetHash(content, ref hashContent);
-            rsa.SignatureFormatter(sprivateKey, hashContent, ref hashContentSignature);
-            var result = rsa.SignatureDeformatter(spublicKey, hashContent, hashContentSignature);
+            rsa.RsaKey(out sPrivateKey,out sPublicKey); //生成密钥
+            rsa.GetHash(content, ref hashContent);  //Hash值
+            rsa.SignatureFormatter(sPrivateKey, hashContent, ref hashContentSignature);  //签名
+            var result = rsa.SignatureDeformatter(sPublicKey, hashContent, hashContentSignature);  //验证签名
             Assert.AreEqual(result,true);
 
         }
