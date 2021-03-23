23a69b8fd4d43cbb3da6b02c15ff227161a26757
chao <chao@DESKTOP-UQQ425Q>
2021-03-23 11:40:10 +0800

    xx公司产品签名

---
+++ NET.Utility/SignApi.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.Utilities
+{
+    /// <summary>
+    /// 签名公司
+    /// </summary>
+    public enum CompanyProduct
+    {
+        /// <summary>
+        /// 万科集团智慧工地智慧工地E劳务系统
+        /// </summary>
+        Vanke_E_LaborService
+    }
+
+
+    public class SignApi
+    {
+        CompanyProduct Product;
+        /// <summary>
+        /// 接口签名
+        /// </summary>
+        /// <param name="product"></param>
+        public SignApi(CompanyProduct product)
+        {
+            Product = product;
+        }
+
+        /// <summary>
+        /// 接口签名
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="postData"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string UrlFormat(string url, string postData, string key)
+        {
+            string result = string.Empty;
+            switch (Product)
+            {
+                case CompanyProduct.Vanke_E_LaborService:
+                    string base64 = Base64Helper.Base64Encode(postData);
+                    string sign = base64 + $",key={key}";
+                    string signed = MD5Helper.MD5(sign);
+                    result = url + $"?sign={signed}";
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ NET.Utility/接口加密/MD5Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.Utilities
+{
+    public class MD5Helper
+    {
+        // 格式化md5 hash 字节数组所用的格式（两位小写16进制数字） 
+        private static readonly string hexFormat = "x2";
+        private MD5Helper() { }
+
+        public static string MD5(string str)
+        {
+            return MD5(str, Encoding.UTF8);
+        }
+
+        public static string MD5(string str, Encoding encodeType)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            var byteValue = encodeType.GetBytes(str);
+            var hashValue = md5.ComputeHash(byteValue);
+            md5.Clear();
+            var sb = new StringBuilder();
+            for (int i = 0; i < hashValue.Length; i++)
+            {
+                sb.Append(hashValue[i].ToString(hexFormat));
+            }
+            return sb.ToString();
+        }
+    }
+}
